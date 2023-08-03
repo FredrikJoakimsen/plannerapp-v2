0d8aecf6ddf3f767ef35abd81a5830ee0e0f6cb3
FredrikJoakimsen <fjoakimsen.408@gmail.com>
2023-08-03 15:20:22 +0200

    implement plans api logic

---
+++ src/PlannerApp.Shared/Models/PlanSummary.cs
@@ -0,0 +1,12 @@
+﻿namespace PlannerApp.Shared.Models
+{
+    public class PlanSummary
+    {
+
+        public string Id { get; set; }
+        public string Tittle { get; set; }
+        public string Description { get; set; }
+        public string CoverUrl { get; set; }
+        //TODO: add additional properties
+    }
+}
